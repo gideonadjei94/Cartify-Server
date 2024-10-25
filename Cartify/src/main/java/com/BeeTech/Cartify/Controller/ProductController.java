9e722e854058f78f9dc1b43fbdffdfac406ebc53
gideonadjei <gideonadjei94@gmail.com>
2024-10-25 18:50:03 +0000

    Initial Commit

---
+++ Cartify/src/main/java/com/BeeTech/Cartify/Controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.BeeTech.Cartify.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ProductController {
+
+    @RequestMapping
+    public String greet(){
+        return "Hello, Welcome to BeeTech";
+    }
+
+}
---
+++ Cartify/src/test/java/com/BeeTech/Cartify/CartifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BeeTech.Cartify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
