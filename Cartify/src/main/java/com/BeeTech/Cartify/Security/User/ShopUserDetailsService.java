89740f0f301ad579575763691212b68da1e32f9a
gideonadjei <gideonadjei94@gmail.com>
2025-01-15 03:37:25 +0000

    Spring Security Implementation

--- Cartify/src/main/java/com/BeeTech/Cartify/Security/User/ShopUserDetailsService.java
+++ Cartify/src/main/java/com/BeeTech/Cartify/Security/User/ShopUserDetailsService.java
@@ -1,4 +1,25 @@
 package com.BeeTech.Cartify.Security.User;
 
-public class ShopUserDetailsService {
+import com.BeeTech.Cartify.Exceptions.ResourceNotFoundException;
+import com.BeeTech.Cartify.Model.User;
+import com.BeeTech.Cartify.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ShopUserDetailsService  implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = Optional.ofNullable(userRepository.findByEmail(email))
+                .orElseThrow( () -> new ResourceNotFoundException("User not Found"));
+        return ShopUserDetails.buildUserDetails(user);
+    }
 }
