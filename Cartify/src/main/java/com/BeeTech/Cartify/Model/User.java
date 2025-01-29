a1292dc44d2a898d22232698b6e0e1ace1b17a1b
gideonadjei <gideonadjei94@gmail.com>
2025-01-29 09:19:12 +0000

    Bug Fixes

--- Cartify/src/main/java/com/BeeTech/Cartify/Model/User.java
+++ Cartify/src/main/java/com/BeeTech/Cartify/Model/User.java
@@ -16,6 +16,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
